d6280fadcedc70fb2d31d47b053809955aa511ce
mkhong210 <ghdalsrud210@gmail.com>
2023-11-17 13:20:53 +0900

    23.11.17

--- src/components/main/Sec3Study.js
+++ src/components/main/Sec3Study.js
@@ -1,35 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import studyData from '../../data/study.json'
 
 function Sec3Study() {
+	const [data, setData] = useState(studyData);
+	const [loading, setLoading] = useState(true);
+	
+	useEffect(() => {
+		setData(studyData)
+		setLoading(false);
+	}, [])
+	// console.log(data.project);
+
+	if(loading){
+		return(<p>로딩중</p>)
+	}
+
 	return (
 		<div className='inner'>
 			<div className='sub_title'>
 				<h3>STUDY</h3>
 			</div>
 			<div className='cont_wrap'>
-				<ul>
-					<li className='study_item'>
-						{/* <Link to='/'> */}
-						<a href='javascrip:;' className='item'>
-							<div className='item_title'>
-								<p>01</p>
-								<p>클론 프로젝트</p>
-							</div>
-							<p>기존의 smdecor 사이트를 클론 프로젝트로 똑같이 구현하는 과정을 진행하였습니다.기존의 smdecor 사이트를 클론 프로젝트로 똑같이 구현하는 과정을 진행하였습니다.</p>
-						</a>
-					</li>
-					<li className='study_item'>
-						{/* <Link to='/'> */}
-						<a href='javascrip:;' className='item'>
-							<div className='item_title'>
-								<p>02</p>
-								<p>Todo List</p>
-							</div>
-							<p>기존의 smdecor 사이트를 클론 프로젝트로 똑같이 구현하는 과정을 진행하였습니다.기존의 smdecor 사이트를 클론 프로젝트로 똑같이 구현하는 과정을 진행하였습니다.</p>
-						</a>
-					</li>
-				</ul>
+				{
+					<ul>
+						{ data.study && data.study.map((obj, k) => (
+							<li className='study_item'>
+								<Link to={obj.giturl} className='item'>
+									<div className='item_title'>
+										<p>0{k+1}</p>
+										<p>{obj.title}</p>
+									</div>
+									<p>{obj.about_desc}</p>
+								</Link>
+							</li>
+						))
+						}
+					</ul>
+				}
 			</div>
 		</div>
 	)
